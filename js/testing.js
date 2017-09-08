e78e6babf6c4ab8378ccbc93887e94ca4ffd1c8f
Mike <mike@none.com>
2017-09-08 10:33:51 -0500

    prototype with knockout and separation of concerns

---
+++ js/testing.js
@@ -0,0 +1,14 @@
+﻿let vm = {
+    showPlaces: ko.observable(true),
+    showDetails: ko.observable(false),
+
+    toggleSidebar: function () {
+        this.showPlaces(!this.showPlaces());
+    },
+
+    toggleDetails: function () {
+        this.showDetails(!this.showDetails());
+    }
+}
+
+ko.applyBindings(vm);
