86c0f3c0f53bb70ff11075bed956e2261c70972a
GW-GhostWolf <mike@none.com>
2017-09-11 23:16:39 -0500

    add initial map with markers for each location

---
+++ js/googleMap.js
@@ -0,0 +1,34 @@
+﻿"use strict";
+var map = map || {};
+
+map.initMap = function () {
+    map.googleMap = new google.maps.Map(document.getElementById('map'), {
+        center: { lat: 29.4241219, lng: -98.4936282 },
+        zoom: 12
+    });
+    //var geocoder = new google.maps.Geocoder();
+    //function geocodeReturn(results, status) {
+    //    if (status == google.maps.GeocoderStatus.OK) {
+    //        this.latlng = results[0].geometry.location;
+    //        this.marker = new google.maps.Marker({
+    //            position: this.latlng,
+    //            map: map.googleMap
+    //        });
+    //        console.log(this);
+    //    } else {
+    //        alert('Geocode was not successful for the following reason: ' + status);
+    //    }
+    //}
+    app.PlaceList().forEach((place) => {
+        //if (!place.latlng) {
+        //    geocoder.geocode({ "address": place.street + ", " + place.city + ", " + place.state }, (results, status) => {
+        //        geocodeReturn.call(place, results, status);
+        //    });
+        //} else {
+            place.marker = new google.maps.Marker({
+                position: place.latlng,
+                map: map.googleMap
+            });
+        //}
+    });
+};
