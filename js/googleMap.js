516a8ec7c13a6d260d5db931eac8c8529de038e5
GW-GhostWolf <mike@none.com>
2017-09-19 20:35:54 -0500

    select place when marker is clicked

--- js/googleMap.js
+++ js/googleMap.js
@@ -14,6 +14,7 @@ map.initMap = function () {
             icon: "https://maps.google.com/mapfiles/ms/icons/red-dot.png",
             map: map.googleMap
         });
+        place.marker.addListener("click", () => { app.selectPlace(place); });
     });
     map.googleMap.fitBounds(map.bounds);
 };
