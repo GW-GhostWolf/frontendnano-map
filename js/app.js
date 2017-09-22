6db4dace435177afc4710c3cc6f099d5c6fa12b1
GW-GhostWolf <mike@none.com>
2017-09-22 14:54:58 -0500

    add filter by attraction type

--- js/app.js
+++ js/app.js
@@ -9,6 +9,7 @@ app.showPlaces = ko.observable(false);
 document.body.onresize();
 app.showDetails = ko.observable(false);
 app.placeFilter = ko.observable("");
+app.categoryFilter = ko.observableArray();
 app.priorPlace = ko.observable();
 
 app.PlaceList = ko.observableArray([]);
@@ -33,20 +34,24 @@ app.selectPlace = function (selectedPlace) {
     selectedPlace.marker.setIcon("https://maps.google.com/mapfiles/ms/icons/green-dot.png");
     selectedPlace.selected(true);
     $.getJSON("https://gw-ghostwolf.github.io/frontendnano-map/data/" + selectedPlace.dataLink + ".json", (data) => {
-        map.infoWindow.setContent("<h3 class='remove-margin'>" + selectedPlace.name + "</h4>" +
-            data.what_it_is + "<br />" +
+        map.infoWindow.setContent("<div class='line-padding'>" + 
+            "<h3 class='remove-margin'>" + selectedPlace.name + "</h4>" +
+            "<span class='large-text'>" + data.what_it_is + "</span><br />" +
             (data.not_to_be_missed ? "This is a MUST DO!<br />" : "") +
             (data.intense ? "" : "Not ") + "Intense, " + (data.frightening ? "" : "Not ") + "Frightening <br />" +
             (data.height_restriction ? "Minimum Height: " + data.height_restriction + "\" <br />" : "") +
             "<a href='javascript:app.toggleDetails();'>Show Pictures</a><br />" +
-            "<span class='small-text'>* Data courtesy of <a href='https://touringplans.com/magic-kingdom/attractions/" + selectedPlace.dataLink + "' target='_blank'>Touring Plans</a></span>");
+            "<span class='small-text'>* Data courtesy of <a href='https://touringplans.com/magic-kingdom/attractions/" + selectedPlace.dataLink + "' target='_blank'>Touring Plans</a></span>" + 
+            "</div>");
         map.infoWindow.open(map.googleMap, selectedPlace.marker);
     });
 };
 
 app.filteredPlaces = ko.computed(function () {
     return ko.utils.arrayFilter(app.PlaceList(), function (place) {
-        let showInList = place.name.toLowerCase().indexOf(app.placeFilter().toLowerCase()) > -1;
+        let showInList =
+            place.name.toLowerCase().indexOf(app.placeFilter().toLowerCase()) > -1
+            && (app.categoryFilter().length === 0 || app.categoryFilter().join(",").indexOf(place.category) > -1);
         if (showInList) {
             if (place.marker && !place.marker.map) { place.marker.setMap(map.googleMap); }
         } else {
